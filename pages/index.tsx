44f138425259e38101b81b199fe41c4a1b9cfb0e
darkdreams77 <x.dark-dreams@hotmail.fr>
2021-11-20 11:02:18 +0100

    ssg to csr + fix styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -10,17 +11,28 @@ interface HomeProps {
   data: FactResponse
 }
 
-const Home = ({ data }: HomeProps) => {
+const Home = () => {
+  const [catFact, setCatFact] = useState<string>('')
+
+  useEffect(() => {
+    axios
+      .get('https://catfact.ninja/fact')
+      .then((res) => {
+        setCatFact(res.data.fact);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <div style={{ margin: '50px auto', width: '700px' }}>
-      <Box sx={{ width: 700 }}>
+    <div style={{ margin: '50px auto', maxWidth: '700px', width: '100%' }}>
+      <Box sx={{ width: '100%' }}>
         <Card variant="outlined">
           <CardContent>
             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
               Cat Quote of the Moment
             </Typography>
             <Typography variant="h5" component="div">
-              {data.fact}
+              {catFact}
             </Typography>
           </CardContent>
         </Card>
@@ -30,20 +42,20 @@ const Home = ({ data }: HomeProps) => {
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`https://catfact.ninja/fact`)
-  const data: FactResponse = await res.json()
+// export async function getStaticProps() {
+//   const res = await fetch(`https://catfact.ninja/fact`)
+//   const data: FactResponse = await res.json()
 
-  if (!data) {
-    return {
-      notFound: true,
-    }
-  }
+//   if (!data) {
+//     return {
+//       notFound: true,
+//     }
+//   }
 
 
-  return {
-    props: { data }, // will be passed to the page component as props
-  }
-}
+//   return {
+//     props: { data }, // will be passed to the page component as props
+//   }
+// }
 
 export default Home
