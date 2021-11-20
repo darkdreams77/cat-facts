2ec94471e4abf21f29c866c867f88ac8fa49f372
darkdreams77 <x.dark-dreams@hotmail.fr>
2021-11-20 19:17:42 +0100

    add random pictures of cats

---
+++ components/Fact/index.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Box } from '@mui/system'
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@mui/material'
+
+const Fact = () => {
+  const [catFact, setCatFact] = useState<string>('')
+  const [catImage, setCatImage] = useState<any>('')
+
+  useEffect(() => {
+    axios
+      .get('https://catfact.ninja/fact')
+      .then((res) => {
+        setCatFact(res.data.fact)
+      })
+      .catch((error) => console.error(error))
+
+    axios
+      .get('https://api.thecatapi.com/v1/images/search', {
+        params: {
+          'x-api-key': '44f356e7-1ed2-468c-93e6-24428117c37d',
+        },
+      })
+      .then((res) => {
+        setCatImage(res.data[0])
+      })
+      .catch((error) => console.error(error))
+  }, [])
+
+  return (
+    <Card sx={{ maxWidth: 345, width: '100%' }}>
+      <CardMedia component="img" height="140" src={catImage.url} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Cat fact of the moment
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {catFact}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Fact
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import React from 'react'
 
-import { FactResponse } from '../types/entities'
-
-interface HomeProps {
-  data: FactResponse
-}
+import Fact from '../components/Fact'
+import { Layout } from '../components/Layout/styles'
 
 const Home = () => {
-  const [catFact, setCatFact] = useState<string>('')
-
-  useEffect(() => {
-    axios
-      .get('https://catfact.ninja/fact')
-      .then((res) => {
-        setCatFact(res.data.fact);
-      })
-      .catch((error) => console.error(error));
-  }, []);
-
   return (
-    <div style={{ margin: '50px auto', maxWidth: '700px', width: '100%' }}>
-      <Box sx={{ width: '100%' }}>
-        <Card variant="outlined">
-          <CardContent>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              Cat Quote of the Moment
-            </Typography>
-            <Typography variant="h5" component="div">
-              {catFact}
-            </Typography>
-          </CardContent>
-        </Card>
-      </Box>
-    </div>
-
+    <>
+      <Layout>
+        <Fact />
+      </Layout>
+    </>
   )
 }
-
-// export async function getStaticProps() {
-//   const res = await fetch(`https://catfact.ninja/fact`)
-//   const data: FactResponse = await res.json()
-
-//   if (!data) {
-//     return {
-//       notFound: true,
-//     }
-//   }
-
-
-//   return {
-//     props: { data }, // will be passed to the page component as props
-//   }
-// }
 
 export default Home
