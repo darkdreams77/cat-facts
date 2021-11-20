667da18b58ef6f4ecae3524d4ec279e762f35115
darkdreams77 <x.dark-dreams@hotmail.fr>
2021-11-20 02:42:47 +0100

    add styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 import { FactResponse } from '../types/entities'
@@ -14,14 +12,21 @@ interface HomeProps {
 
 const Home = ({ data }: HomeProps) => {
   return (
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Cat Quote of the Moment
-      </Typography>
-      <Typography variant="h5" component="div">
-        {data.fact}
-      </Typography>
-    </CardContent>
+    <div style={{ margin: '50px auto', width: '700px' }}>
+      <Box sx={{ width: 700 }}>
+        <Card variant="outlined">
+          <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              Cat Quote of the Moment
+            </Typography>
+            <Typography variant="h5" component="div">
+              {data.fact}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+    </div>
+
   )
 }
 
